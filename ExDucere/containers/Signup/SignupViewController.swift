64e214e7ae311d4eb37c2393b0706d50d29c8ff7
Pedro Rossi <pgrr@cin.ufpe.br>
2017-11-28 10:37:31 -0300

    Firebase config fixed and some screens done for better guiding

---
+++ ExDucere/containers/Signup/SignupViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SignupViewController.swift
+//  ExDucere
+//
+//  Created by Pedro Gonçalves Rossi Rodrigues on 28/11/17.
+//  Copyright © 2017 Pedro Gonçalves Rossi Rodrigues. All rights reserved.
+//
+
+import UIKit
+
+class SignupViewController: UIViewController {
+
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(false, animated: false)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
