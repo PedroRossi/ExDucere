42f915bdc7e466c3861f2b2c97aedd16a7a3cf12
Pedro Gonçalves Rossi Rodrigues <pgrr@brufpe-09.local>
2017-11-28 09:32:40 -0300

    Initial Commit

---
+++ ExDucereTests/ExDucereTests.swift
@@ -0,0 +1,36 @@
+//
+//  ExDucereTests.swift
+//  ExDucereTests
+//
+//  Created by Pedro Gonçalves Rossi Rodrigues on 28/11/17.
+//  Copyright © 2017 Pedro Gonçalves Rossi Rodrigues. All rights reserved.
+//
+
+import XCTest
+@testable import ExDucere
+
+class ExDucereTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
