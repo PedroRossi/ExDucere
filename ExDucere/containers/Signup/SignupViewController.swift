8c37b21ea0d0a1b9503176f5670605cea4a2ccfb
Pedro Henrique Martins Barbosa <phmb2@brufpe-17.local>
2017-12-05 13:00:25 -0300

    cadastro usuario (senha/e-mail) no firebase

--- ExDucere/containers/Signup/SignupViewController.swift
+++ ExDucere/containers/Signup/SignupViewController.swift
@@ -7,11 +7,83 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class SignupViewController: UIViewController {
 
+    @IBOutlet weak var nome: UITextField!
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var senha: UITextField!
+    @IBOutlet weak var senhaConfirmada: UITextField!
+    
+    @IBAction func cadastrarUsuario(_ sender: Any) {
+        
+        //Recuperar dados digitados
+        //if let nomeRecuperado = self.nome.text{
+        if let emailRecuperado = self.email.text{
+            if let senhaRecuperada = self.senha.text{
+                if let confirmarSenhaRecuperada = self.senhaConfirmada.text{
+                    
+                    //Validar Senha
+                    if senhaRecuperada == confirmarSenhaRecuperada{
+                        let autenticacao = Auth.auth()
+                        
+                        autenticacao.createUser(withEmail: emailRecuperado, password: senhaRecuperada){
+                            (usuario, erro) in
+                            if erro == nil{
+                                print("Sucesso ao cadastrar usuário.")
+                            }
+                            else{
+                                
+                                let erroRecuperado = erro! as NSError
+                                if let codigoErro = erroRecuperado.userInfo["error_name"]{
+                                    
+                                    let erroTexto = codigoErro as! String
+                                    var mensagemErro = ""
+                                    
+                                    switch erroTexto{
+                                        
+                                    case "ERROR_INVALID_EMAIL":
+                                        mensagemErro = "E-mail inválido, digite um e-mail válido"
+                                        break
+                                    case "ERROR_EMAIL_ALREADY_IN_USE":
+                                        mensagemErro = "Esse e-mail já está sendo utilizado inválido, crie sua conta com outro e-mail."
+                                        break
+                                    case "ERROR_WEAK_PASSWORD":
+                                        mensagemErro = "Senha precisa ter no mínimo 6 caracteres, com letras e números."
+                                        break
+                                    default:
+                                        mensagemErro = "Dados digitados estão incorretos."
+                                        
+                                    }
+                                    
+                                    self.exibirMensagem(titulo: "Dados inválidos", mensagem: mensagemErro)
+                                }
+                            }
+                        }
+                    }
+                    else{
+                        self.exibirMensagem(titulo: "Dados Incorretos", mensagem: "As senhas não estão iguais, digite novamente.")
+                    }
+                    
+                }
+            }
+        }
+        //}
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
         self.navigationController?.setNavigationBarHidden(false, animated: false)
+    }
+    
+    func exibirMensagem(titulo: String, mensagem: String){
+        
+        let alerta = UIAlertController(title: titulo, message: mensagem, preferredStyle: .alert)
+        
+        let acaoCancelar = UIAlertAction(title: "Cancelar", style: .cancel , handler: nil)
+        
+        alerta.addAction(acaoCancelar)
+        present(alerta, animated: true, completion: nil)
     }
     
     override func viewDidLoad() {
