78e226b94443f0010419aeaf800cdbe8d00c476b
Pedro Rossi <pgrr@cin.ufpe.br>
2017-12-07 10:50:58 -0300

    Signup saving on db

--- ExDucere/containers/Signup/SignupViewController.swift
+++ ExDucere/containers/Signup/SignupViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import FirebaseAuth
+import FirebaseDatabase
 
 class SignupViewController: UIViewController {
 
@@ -19,57 +20,65 @@ class SignupViewController: UIViewController {
     @IBAction func cadastrarUsuario(_ sender: Any) {
         
         //Recuperar dados digitados
-        //if let nomeRecuperado = self.nome.text{
-        if let emailRecuperado = self.email.text{
-            if let senhaRecuperada = self.senha.text{
-                if let confirmarSenhaRecuperada = self.senhaConfirmada.text{
-                    
-                    //Validar Senha
-                    if senhaRecuperada == confirmarSenhaRecuperada{
-                        let autenticacao = Auth.auth()
+        if let nomeRecuperado = self.nome.text{
+            if let emailRecuperado = self.email.text{
+                if let senhaRecuperada = self.senha.text{
+                    if let confirmarSenhaRecuperada = self.senhaConfirmada.text{
                         
-                        autenticacao.createUser(withEmail: emailRecuperado, password: senhaRecuperada){
-                            (usuario, erro) in
-                            if erro == nil{
-                                print("Sucesso ao cadastrar usuário.")
-                            }
-                            else{
-                                
-                                let erroRecuperado = erro! as NSError
-                                if let codigoErro = erroRecuperado.userInfo["error_name"]{
+                        //Validar Senha
+                        if senhaRecuperada == confirmarSenhaRecuperada{
+                            
+                            Auth.auth().createUser(withEmail: emailRecuperado, password: senhaRecuperada){
+                                (user, erro) in
+                                if erro == nil{
+                                    print("Sucesso ao cadastrar usuário.")
+                                    var ref: DatabaseReference!
+                                    ref = Database.database().reference()
+                                    ref.child("users").child((user?.uid)!).observeSingleEvent(of: .value, with: { (snapshot) in
+                                        if snapshot.exists() {
+                                            
+                                        } else {
+                                            ref.child("users").child((user?.uid)!).setValue(["name": nomeRecuperado])
+                                        }
+                                    })
+                                }
+                                else{
                                     
-                                    let erroTexto = codigoErro as! String
-                                    var mensagemErro = ""
-                                    
-                                    switch erroTexto{
+                                    let erroRecuperado = erro! as NSError
+                                    if let codigoErro = erroRecuperado.userInfo["error_name"]{
                                         
-                                    case "ERROR_INVALID_EMAIL":
-                                        mensagemErro = "E-mail inválido, digite um e-mail válido"
-                                        break
-                                    case "ERROR_EMAIL_ALREADY_IN_USE":
-                                        mensagemErro = "Esse e-mail já está sendo utilizado inválido, crie sua conta com outro e-mail."
-                                        break
-                                    case "ERROR_WEAK_PASSWORD":
-                                        mensagemErro = "Senha precisa ter no mínimo 6 caracteres, com letras e números."
-                                        break
-                                    default:
-                                        mensagemErro = "Dados digitados estão incorretos."
+                                        let erroTexto = codigoErro as! String
+                                        var mensagemErro = ""
                                         
+                                        switch erroTexto{
+                                            
+                                        case "ERROR_INVALID_EMAIL":
+                                            mensagemErro = "E-mail inválido, digite um e-mail válido"
+                                            break
+                                        case "ERROR_EMAIL_ALREADY_IN_USE":
+                                            mensagemErro = "Esse e-mail já está sendo utilizado inválido, crie sua conta com outro e-mail."
+                                            break
+                                        case "ERROR_WEAK_PASSWORD":
+                                            mensagemErro = "Senha precisa ter no mínimo 6 caracteres, com letras e números."
+                                            break
+                                        default:
+                                            mensagemErro = "Dados digitados estão incorretos."
+                                            
+                                        }
+                                        
+                                        self.exibirMensagem(titulo: "Dados inválidos", mensagem: mensagemErro)
                                     }
-                                    
-                                    self.exibirMensagem(titulo: "Dados inválidos", mensagem: mensagemErro)
                                 }
                             }
                         }
+                        else{
+                            self.exibirMensagem(titulo: "Dados Incorretos", mensagem: "As senhas não estão iguais, digite novamente.")
+                        }
+                        
                     }
-                    else{
-                        self.exibirMensagem(titulo: "Dados Incorretos", mensagem: "As senhas não estão iguais, digite novamente.")
-                    }
-                    
                 }
             }
         }
-        //}
     }
     
     override func viewWillAppear(_ animated: Bool) {
